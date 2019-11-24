0186bb74d4a6f930bffa760cc294a73d2c9028fa
RufinaMay <rufa-gali@mail.ru>
2019-11-24 22:38:15 +0300

    RNN for MLC

--- models/mlc/mlc.py
+++ models/mlc/mlc.py
@@ -32,26 +32,26 @@ class MultilabelClassification():
         # return model
 
         model = Sequential([
-            TimeDistributed(Conv2D(filters=64, kernel_size=(3, 3), activation="relu"), input_shape=(1, 448, 448, 3)),
+            TimeDistributed(Conv2D(filters=64, kernel_size=(3, 3), activation="tanh"), input_shape=(1, 448, 448, 3)),
             TimeDistributed(Conv2D(filters=64, kernel_size=(3, 3), activation='relu')),
             TimeDistributed(MaxPooling2D(pool_size=(2, 2))),
             # Dropout(0.25),
-            TimeDistributed(Conv2D(filters=128, kernel_size=(3, 3), activation='relu')),
+            TimeDistributed(Conv2D(filters=128, kernel_size=(3, 3), activation='tanh')),
             TimeDistributed(Conv2D(filters=128, kernel_size=(3, 3), activation='relu')),
             TimeDistributed(MaxPooling2D(pool_size=(2, 2))),
             # Dropout(0.25),
-            TimeDistributed(Conv2D(filters=256, kernel_size=(3, 3), activation="relu")),
-            TimeDistributed(Conv2D(filters=256, kernel_size=(3, 3), activation="relu")),
+            TimeDistributed(Conv2D(filters=256, kernel_size=(3, 3), activation="tanh")),
+            TimeDistributed(Conv2D(filters=256, kernel_size=(3, 3), activation="tanh")),
             TimeDistributed(Conv2D(filters=256, kernel_size=(1, 1), activation="relu")),
             TimeDistributed(MaxPooling2D(pool_size=(2, 2))),
             # Dropout(0.25),
-            TimeDistributed(Conv2D(filters=512, kernel_size=(3, 3), activation="relu")),
-            TimeDistributed(Conv2D(filters=512, kernel_size=(3, 3), activation="relu")),
-            TimeDistributed(Conv2D(filters=512, kernel_size=(1, 1), activation="relu")),
+            TimeDistributed(Conv2D(filters=512, kernel_size=(3, 3), activation="tanh")),
+            TimeDistributed(Conv2D(filters=512, kernel_size=(3, 3), activation="tanh")),
+            TimeDistributed(Conv2D(filters=512, kernel_size=(1, 1), activation="tanh")),
             TimeDistributed(MaxPooling2D(pool_size=(2, 2))),
             # Dropout(0.25),
             TimeDistributed(Flatten()),
-            LSTM(100, return_sequences=True),
+            LSTM(8, return_sequences=True),
             Dense(UNIQUE_TAGS, activation='sigmoid')
          ])
 
