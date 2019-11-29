c2aa3174b5b3036f7b45929ea0d92d4d057fa185
RufinaMay <rufa-gali@mail.ru>
2019-11-29 18:09:55 +0300

    cutting in labels for indiana chest x ray to 559

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # import os
-# import numpy as np
-# from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib import pyplot as plt
 import pickle
 # from utils.constants import IMG_DIR, PATH_IMG_TAG_MAPPING
 # from utils.utils import read_and_resize
@@ -19,19 +19,19 @@ from preprocessing.chest_xray_extractor import process_all_reports
 IMG_REPORT, IMG_TAG, TAG_VOCAB, WORD_VOCAB, TAG_IMAGESNUMB = process_all_reports('data/chest_reports/ecgen-radiology')
 print(len(TAG_IMAGESNUMB))
 print(TAG_IMAGESNUMB.most_common(10))
-print(TAG_IMAGESNUMB.most_common()[-20:])
+print(TAG_IMAGESNUMB.most_common()[:-15])
 
 new_tag_vocab = set()
 total = 0
 part = 0
 for tag in TAG_IMAGESNUMB:
     total += TAG_IMAGESNUMB[tag]
-    if TAG_IMAGESNUMB[tag] >= 16:
+    if TAG_IMAGESNUMB[tag] >= 1:
         new_tag_vocab.add(tag)
         part += TAG_IMAGESNUMB[tag]
 print(part, ' ', total)
 print(part/total)
 print(len(new_tag_vocab))
 
-# with open('tag_vocab_6.pickle', 'wb') as f:
+# with open('tag_vocab_559.pickle', 'wb') as f:
 #     pickle.dump(new_tag_vocab, f)
