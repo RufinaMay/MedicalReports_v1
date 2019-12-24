e7d54afbc65823cce18cea7cb075e82f617b7575
RufinaMay <rufa-gali@mail.ru>
2019-12-24 11:37:10 +0300

    remove tags that occur less than 30 times

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pickle
+from collections import Counter
 
 from preprocessing.chest_xray_extractor import process_all_reports
 from preprocessing.train_test_split import run_split
@@ -16,3 +17,25 @@ with open('tag_vocab_all.pickle', 'wb') as f:
 print('after IMG tag size ', len(IMG_TAG))
 
 run_split()
+
+# remove tags from train dataset that occur less than 20 times
+with open('train_set.pickle', 'rb') as f:
+    train = pickle.load(f)
+
+tag_occurrences = Counter()
+for img in train:
+    for tag in train[img]:
+        tag_occurrences[tag] += 1
+
+tag_vocab = set()
+for tag in tag_occurrences:
+    if tag_occurrences[tag] >= 30:
+        tag_vocab.add(tag)
+
+print(f'total tags considered in train set {len(tag_vocab)}')
+
+# change tag to index file according to the tags taken into account from training set
+TAG_TO_INDEX = {tag: i for i, tag in enumerate(tag_vocab)}
+
+with open('TAG_TO_INDEX.pickle', 'wb') as f:
+    pickle.dump(TAG_TO_INDEX, f)
